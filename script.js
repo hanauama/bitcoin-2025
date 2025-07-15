c7b8335c24a6282c5b91404bd7bd40441fe77b86
hanauama <65953470+hanauama@users.noreply.github.com>
2025-07-15 21:13:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ async function drawChart() {
     y: weighted,
     mode: "lines+markers",
     name: "Średnia ważona",
-    line: { color: "rgba(0,0,0,0.6)", width: 4 },
+    line: { color: "rgba(0,0,0,0.8)", width: 6 },
     hovertemplate: '%{x|%d %B}: %{y}$<extra></extra>'
   };
 
